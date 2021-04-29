7578fe14d364cbc07cdda4c7c163b75d3fdfb5bf
Kanchan Shaha <k.saha5767@gmail.com>
2021-04-29 22:52:31 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// Falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+
+// Truthy
+// '0'  ' '
+
+let name = 0;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
