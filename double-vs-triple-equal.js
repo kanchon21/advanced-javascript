d2fe3a6a37b51acd80f60051465745fe3166e9b2
Kanchan Shaha <k.saha5767@gmail.com>
2021-04-30 09:51:25 +0600

    Learn Map, Filter and Find

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,8 @@
+const first = 1;
+const second = true;
+if('2' === 2){
+    console.log("inside if");
+}
+else{
+    console.log('inside else');
+}
--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,14 +1,14 @@
-// let num;
+let num;
 
-// function add(num1, num2){
-//     console.log(num1, num2);
-// }
+function add(num1, num2){
+    console.log(num1, num2);
+}
 
-// const premik = { name:"smart dude", phone: 01919222333 };
-// console.log(premik.gf);
+const premik = { name:"smart dude", phone: 01919222333 };
+console.log(premik.gf);
 
-// let fun = undefined;
-// console.log(fun);
+let fun = undefined;
+console.log(fun);
 
 let ages = [3, 5, 9];
 console.log(ages[11]);
